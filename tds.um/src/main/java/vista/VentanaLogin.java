648455cb918599951b314bf4288cd5c5257e9441
Alvaro-tech <56553440+Alvaro-tech@users.noreply.github.com>
2019-11-19 20:24:45 +0100

    Interfaz del login y el registro

---
+++ tds.um/src/main/java/vista/VentanaLogin.java
@@ -0,0 +1,222 @@
+package vista;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.SystemColor;
+import javax.swing.JPanel;
+import java.awt.FlowLayout;
+import javax.swing.JTextField;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.GridLayout;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.Panel;
+import java.awt.TextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.ComponentOrientation;
+import javax.swing.JTextArea;
+import java.awt.Component;
+import javax.swing.JPasswordField;
+
+public class VentanaLogin {
+
+	private JFrame frame;   
+	private JTextField relleno;
+	private JTextField relleno_1;
+	private JPanel panel_Principal;
+	private JTextPane textPane;
+	private JTextPane textPane_1;
+	private JPanel panel_Org;
+	private JTextField textName;
+	private JTextField textName_W;
+	private JPasswordField textPasswrd_W;
+	private JTextField txtPassword;
+	private Component verticalStrut;
+	private Component verticalStrut_1;
+	private Component verticalStrut_3;
+	private Component verticalStrut_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaLogin window = new VentanaLogin();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public VentanaLogin() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JTextPane textBienvenida = new JTextPane();
+		textBienvenida.setEditable(false);
+		textBienvenida.setForeground(SystemColor.textHighlight);
+		textBienvenida.setBackground(SystemColor.control);
+		textBienvenida.setFont(new Font("Sitka Small", Font.BOLD, 16));
+		textBienvenida.setText("                      Bienvenido/a a AppChat");
+		frame.getContentPane().add(textBienvenida, BorderLayout.NORTH);
+		
+		JPanel panel_1 = new JPanel();
+		frame.getContentPane().add(panel_1, BorderLayout.SOUTH);
+		
+		JButton btnAceptar = new JButton("Aceptar");
+		panel_1.add(btnAceptar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		panel_1.add(btnCancelar);
+		
+		JButton btnRegistro = new JButton("Registro");   //?????????????????????????????????????
+		btnRegistro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				JPanel registroV = new VentanaRegistro(frame); //Esto seria mejor subirlo?
+				frame.setContentPane(registroV);
+				frame.revalidate();
+			}
+		});
+		panel_1.add(btnRegistro);
+		
+		relleno = new JTextField();
+		relleno.setBorder(null);
+		relleno.setBackground(SystemColor.control);
+		relleno.setEnabled(false);
+		frame.getContentPane().add(relleno, BorderLayout.WEST);
+		relleno.setColumns(10);
+		
+		relleno_1 = new JTextField();
+		relleno_1.setBorder(null);
+		relleno_1.setBackground(SystemColor.control);
+		relleno_1.setEnabled(false);
+		frame.getContentPane().add(relleno_1, BorderLayout.EAST);
+		relleno_1.setColumns(10);
+		
+		panel_Principal = new JPanel();
+		frame.getContentPane().add(panel_Principal, BorderLayout.CENTER);
+		panel_Principal.setLayout(new BorderLayout(0, 0));
+		
+		textPane = new JTextPane();
+		textPane.setEditable(false);
+		textPane.setBackground(SystemColor.activeCaption);
+		panel_Principal.add(textPane, BorderLayout.NORTH);
+		
+		textPane_1 = new JTextPane();
+		textPane_1.setEditable(false);
+		textPane_1.setBackground(SystemColor.activeCaption);
+		panel_Principal.add(textPane_1, BorderLayout.SOUTH);
+		
+		panel_Org = new JPanel();
+		panel_Principal.add(panel_Org, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_Org = new GridBagLayout();
+		gbl_panel_Org.columnWidths = new int[]{0, 0, 0, 80, 126, 0, 0, 0, 0};
+		gbl_panel_Org.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_Org.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_Org.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_Org.setLayout(gbl_panel_Org);
+		
+		verticalStrut = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut = new GridBagConstraints();
+		gbc_verticalStrut.fill = GridBagConstraints.BOTH;
+		gbc_verticalStrut.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut.gridx = 3;
+		gbc_verticalStrut.gridy = 0;
+		panel_Org.add(verticalStrut, gbc_verticalStrut);
+		
+		verticalStrut_1 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_1 = new GridBagConstraints();
+		gbc_verticalStrut_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_verticalStrut_1.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_1.gridx = 3;
+		gbc_verticalStrut_1.gridy = 1;
+		panel_Org.add(verticalStrut_1, gbc_verticalStrut_1);
+		
+		textName = new JTextField();
+		textName.setEditable(false);
+		textName.setBorder(null);
+		textName.setBackground(SystemColor.control);
+		textName.setText("Nombre:");
+		GridBagConstraints gbc_textName = new GridBagConstraints();
+		gbc_textName.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textName.insets = new Insets(0, 0, 5, 5);
+		gbc_textName.gridx = 3;
+		gbc_textName.gridy = 2;
+		panel_Org.add(textName, gbc_textName);
+		textName.setColumns(6);
+		
+		textName_W = new JTextField();
+		GridBagConstraints gbc_textName_W = new GridBagConstraints();
+		gbc_textName_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textName_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textName_W.gridx = 4;
+		gbc_textName_W.gridy = 2;
+		panel_Org.add(textName_W, gbc_textName_W);
+		textName_W.setColumns(10);
+		
+		txtPassword = new JTextField();
+		txtPassword.setEditable(false);
+		txtPassword.setBackground(SystemColor.control);
+		txtPassword.setBorder(null);
+		txtPassword.setText("Contraseña:");
+		GridBagConstraints gbc_txtPassword = new GridBagConstraints();
+		gbc_txtPassword.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtPassword.insets = new Insets(0, 0, 5, 5);
+		gbc_txtPassword.gridx = 3;
+		gbc_txtPassword.gridy = 3;
+		panel_Org.add(txtPassword, gbc_txtPassword);
+		txtPassword.setColumns(13);
+		
+		textPasswrd_W = new JPasswordField();
+		GridBagConstraints gbc_textPasswrd_W = new GridBagConstraints();
+		gbc_textPasswrd_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textPasswrd_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textPasswrd_W.gridx = 4;
+		gbc_textPasswrd_W.gridy = 3;
+		panel_Org.add(textPasswrd_W, gbc_textPasswrd_W);
+		textPasswrd_W.setColumns(10);
+		
+		verticalStrut_2 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_2 = new GridBagConstraints();
+		gbc_verticalStrut_2.fill = GridBagConstraints.BOTH;
+		gbc_verticalStrut_2.insets = new Insets(0, 0, 5, 5);
+		gbc_verticalStrut_2.gridx = 3;
+		gbc_verticalStrut_2.gridy = 4;
+		panel_Org.add(verticalStrut_2, gbc_verticalStrut_2);
+		
+		verticalStrut_3 = Box.createVerticalStrut(20);
+		GridBagConstraints gbc_verticalStrut_3 = new GridBagConstraints();
+		gbc_verticalStrut_3.fill = GridBagConstraints.BOTH;
+		gbc_verticalStrut_3.insets = new Insets(0, 0, 0, 5);
+		gbc_verticalStrut_3.gridx = 3;
+		gbc_verticalStrut_3.gridy = 5;
+		panel_Org.add(verticalStrut_3, gbc_verticalStrut_3);
+	}
+
+}
---
+++ tds.um/src/main/java/vista/VentanaRegistro.java
@@ -0,0 +1,241 @@
+package vista;
+
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+import javax.swing.JTextField;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextArea;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingConstants;
+import javax.swing.JTextPane;
+import java.awt.SystemColor;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class VentanaRegistro extends JPanel {
+	private JFrame ventana;
+	private JPanel jpanelAnterior;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textNombre;
+	private JTextField textPhone;
+	private JTextField txtFnacimiento;
+	private JTextField textEmail;
+	private JTextField txtUsuario;
+	private JTextField txtClave;
+	private JTextField txtConfirmacion;
+	private JTextField textName_W;
+	private JTextField textPhone_W;
+	private JTextField textFnacimiento_W;
+	private JTextField textEmail_W;
+	private JTextField textUsuario_W;
+	private JPasswordField passwordClave_W;
+	private JPasswordField passwordConfirmación_W;
+	
+
+	/**
+	 * Create the panel.
+	 */
+	public VentanaRegistro(JFrame frame) {
+		ventana = frame;
+		jpanelAnterior = (JPanel) ventana.getContentPane();
+		
+		
+		setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_Sur = new JPanel();
+		add(panel_Sur, BorderLayout.SOUTH);
+		
+		JButton btnRegistrar = new JButton("Registrar");
+		panel_Sur.add(btnRegistrar);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ventana.setContentPane(jpanelAnterior);
+				ventana.revalidate();
+			}
+		});
+		panel_Sur.add(btnCancelar);
+		
+		textField = new JTextField();
+		textField.setEnabled(false);
+		textField.setColumns(10);
+		textField.setBorder(null);
+		textField.setBackground(SystemColor.menu);
+		add(textField, BorderLayout.EAST);
+		
+		textField_1 = new JTextField();
+		textField_1.setEnabled(false);
+		textField_1.setColumns(10);
+		textField_1.setBorder(null);
+		textField_1.setBackground(SystemColor.menu);
+		add(textField_1, BorderLayout.WEST);
+		
+		JPanel panel_Central = new JPanel();
+		add(panel_Central, BorderLayout.CENTER);
+		GridBagLayout gbl_panel_Central = new GridBagLayout();
+		gbl_panel_Central.columnWidths = new int[]{58, 91, 98, 55, 0};
+		gbl_panel_Central.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel_Central.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_Central.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		panel_Central.setLayout(gbl_panel_Central);
+		
+		textNombre = new JTextField();
+		textNombre.setEditable(false);
+		textNombre.setText("Nombre:");
+		GridBagConstraints gbc_textNombre = new GridBagConstraints();
+		gbc_textNombre.insets = new Insets(0, 0, 5, 5);
+		gbc_textNombre.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textNombre.gridx = 1;
+		gbc_textNombre.gridy = 0;
+		panel_Central.add(textNombre, gbc_textNombre);
+		textNombre.setColumns(10);
+		
+		textName_W = new JTextField();
+		GridBagConstraints gbc_textName_W = new GridBagConstraints();
+		gbc_textName_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textName_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textName_W.gridx = 2;
+		gbc_textName_W.gridy = 0;
+		panel_Central.add(textName_W, gbc_textName_W);
+		textName_W.setColumns(10);
+		
+		textPhone = new JTextField();
+		textPhone.setText("Teléfono");
+		textPhone.setEditable(false);
+		GridBagConstraints gbc_textPhone = new GridBagConstraints();
+		gbc_textPhone.insets = new Insets(0, 0, 5, 5);
+		gbc_textPhone.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textPhone.gridx = 1;
+		gbc_textPhone.gridy = 1;
+		panel_Central.add(textPhone, gbc_textPhone);
+		textPhone.setColumns(10);
+		
+		textPhone_W = new JTextField();
+		GridBagConstraints gbc_textPhone_W = new GridBagConstraints();
+		gbc_textPhone_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textPhone_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textPhone_W.gridx = 2;
+		gbc_textPhone_W.gridy = 1;
+		panel_Central.add(textPhone_W, gbc_textPhone_W);
+		textPhone_W.setColumns(10);
+		
+		txtFnacimiento = new JTextField();
+		txtFnacimiento.setEditable(false);
+		txtFnacimiento.setText("F.Nacimiento:");
+		GridBagConstraints gbc_txtFnacimiento = new GridBagConstraints();
+		gbc_txtFnacimiento.insets = new Insets(0, 0, 5, 5);
+		gbc_txtFnacimiento.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtFnacimiento.gridx = 1;
+		gbc_txtFnacimiento.gridy = 2;
+		panel_Central.add(txtFnacimiento, gbc_txtFnacimiento);
+		txtFnacimiento.setColumns(10);
+		
+		textFnacimiento_W = new JTextField();
+		GridBagConstraints gbc_textFnacimiento_W = new GridBagConstraints();
+		gbc_textFnacimiento_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textFnacimiento_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFnacimiento_W.gridx = 2;
+		gbc_textFnacimiento_W.gridy = 2;
+		panel_Central.add(textFnacimiento_W, gbc_textFnacimiento_W);
+		textFnacimiento_W.setColumns(10);
+		
+		textEmail = new JTextField();
+		textEmail.setEditable(false);
+		textEmail.setText("Email:");
+		GridBagConstraints gbc_textEmail = new GridBagConstraints();
+		gbc_textEmail.insets = new Insets(0, 0, 5, 5);
+		gbc_textEmail.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textEmail.gridx = 1;
+		gbc_textEmail.gridy = 3;
+		panel_Central.add(textEmail, gbc_textEmail);
+		textEmail.setColumns(10);
+		
+		textEmail_W = new JTextField();
+		GridBagConstraints gbc_textEmail_W = new GridBagConstraints();
+		gbc_textEmail_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textEmail_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textEmail_W.gridx = 2;
+		gbc_textEmail_W.gridy = 3;
+		panel_Central.add(textEmail_W, gbc_textEmail_W);
+		textEmail_W.setColumns(10);
+		
+		txtUsuario = new JTextField();
+		txtUsuario.setEditable(false);
+		txtUsuario.setText("Usuario:");
+		GridBagConstraints gbc_txtUsuario = new GridBagConstraints();
+		gbc_txtUsuario.insets = new Insets(0, 0, 5, 5);
+		gbc_txtUsuario.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUsuario.gridx = 1;
+		gbc_txtUsuario.gridy = 4;
+		panel_Central.add(txtUsuario, gbc_txtUsuario);
+		txtUsuario.setColumns(10);
+		
+		textUsuario_W = new JTextField();
+		GridBagConstraints gbc_textUsuario_W = new GridBagConstraints();
+		gbc_textUsuario_W.insets = new Insets(0, 0, 5, 5);
+		gbc_textUsuario_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textUsuario_W.gridx = 2;
+		gbc_textUsuario_W.gridy = 4;
+		panel_Central.add(textUsuario_W, gbc_textUsuario_W);
+		textUsuario_W.setColumns(10);
+		
+		txtClave = new JTextField();
+		txtClave.setEditable(false);
+		txtClave.setText("Clave:");
+		GridBagConstraints gbc_txtClave = new GridBagConstraints();
+		gbc_txtClave.insets = new Insets(0, 0, 5, 5);
+		gbc_txtClave.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtClave.gridx = 1;
+		gbc_txtClave.gridy = 5;
+		panel_Central.add(txtClave, gbc_txtClave);
+		txtClave.setColumns(10);
+		
+		passwordClave_W = new JPasswordField();
+		GridBagConstraints gbc_passwordClave_W = new GridBagConstraints();
+		gbc_passwordClave_W.insets = new Insets(0, 0, 5, 5);
+		gbc_passwordClave_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordClave_W.gridx = 2;
+		gbc_passwordClave_W.gridy = 5;
+		panel_Central.add(passwordClave_W, gbc_passwordClave_W);
+		
+		txtConfirmacion = new JTextField();
+		txtConfirmacion.setEditable(false);
+		txtConfirmacion.setText("Confirmación:");
+		GridBagConstraints gbc_txtConfirmacion = new GridBagConstraints();
+		gbc_txtConfirmacion.insets = new Insets(0, 0, 0, 5);
+		gbc_txtConfirmacion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtConfirmacion.gridx = 1;
+		gbc_txtConfirmacion.gridy = 6;
+		panel_Central.add(txtConfirmacion, gbc_txtConfirmacion);
+		txtConfirmacion.setColumns(10);
+		
+		passwordConfirmación_W = new JPasswordField();
+		GridBagConstraints gbc_passwordConfirmación_W = new GridBagConstraints();
+		gbc_passwordConfirmación_W.insets = new Insets(0, 0, 0, 5);
+		gbc_passwordConfirmación_W.fill = GridBagConstraints.HORIZONTAL;
+		gbc_passwordConfirmación_W.gridx = 2;
+		gbc_passwordConfirmación_W.gridy = 6;
+		panel_Central.add(passwordConfirmación_W, gbc_passwordConfirmación_W);
+		
+		JPanel panel_Norte = new JPanel();
+		add(panel_Norte, BorderLayout.NORTH);
+		
+		JTextPane txtpnBienvenidoaAAppchat = new JTextPane();
+		txtpnBienvenidoaAAppchat.setText("Bienvenido/a a AppChat");
+		txtpnBienvenidoaAAppchat.setForeground(SystemColor.textHighlight);
+		txtpnBienvenidoaAAppchat.setFont(new Font("Sitka Small", Font.BOLD, 16));
+		txtpnBienvenidoaAAppchat.setEditable(false);
+		txtpnBienvenidoaAAppchat.setBackground(SystemColor.menu);
+		panel_Norte.add(txtpnBienvenidoaAAppchat);
+
+	}
+}
