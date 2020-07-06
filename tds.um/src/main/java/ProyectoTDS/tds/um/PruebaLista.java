8774f3e3f0c94df0501dd3b41e1e8247a7b5178d
Alvaro-tech <56553440+Alvaro-tech@users.noreply.github.com>
2020-07-07 00:39:51 +0200

    Libreria iText 5
    Para gestión de pdf
    
    Página con un ejemplillo
    http://soloinformaticayalgomas.blogspot.com/2010/12/generar-un-documento-pdf-desde-java.html

--- tds.um/src/main/java/ProyectoTDS/tds/um/PruebaLista.java
+++ tds.um/src/main/java/ProyectoTDS/tds/um/PruebaLista.java
@@ -16,10 +16,21 @@ import javax.swing.ListCellRenderer;
 import javax.swing.UIManager;
 import javax.swing.border.EmptyBorder;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 import modelo.Usuario;
 import vista.ContactoRenderer;
 
 import javax.swing.JList;
+
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+
 
 public class PruebaLista extends JFrame {
 
@@ -28,9 +39,15 @@ public class PruebaLista extends JFrame {
 	/**
 	 * Launch the application.
 	 */
-	public static void main(String[] args) {
-	
-	}
+	 public static void main(String[] a3d) throws FileNotFoundException, DocumentException {
+	      FileOutputStream archivo = new FileOutputStream("C:\\hola.pdf");
+	      Document documento = new Document();
+	      PdfWriter.getInstance(documento, archivo);
+	      documento.open();
+	      documento.add(new Paragraph("Hola Mundo!"));
+	      documento.add(new Paragraph("SoloInformaticaYAlgoMas.blogspot.com"));
+	      documento.close();
+	   }
 
 	/**
 	 * Create the frame.
