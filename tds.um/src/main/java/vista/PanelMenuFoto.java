49ba7f2055f2bd99e60c2a57306c2692f960c629
Alvaro-tech <56553440+Alvaro-tech@users.noreply.github.com>
2020-07-09 17:48:01 +0200

    Comit de sincroniczacion

--- tds.um/src/main/java/vista/PanelMenuFoto.java
+++ tds.um/src/main/java/vista/PanelMenuFoto.java
@@ -26,10 +26,9 @@ import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class PanelMenuFoto extends JPanel {
-	private JTextField txtNombre;
-	private JTextField txtSaludo;
 	private JTextField textNameField;
 	private JTextField textSaludoField;
 	private JPanel panelImagen;
@@ -37,8 +36,6 @@ public class PanelMenuFoto extends JPanel {
 	private JPanel panelBotones;
 	private JButton btnCambiarFoto;
 	private JButton btnCambiarSaludo;
-	private JTextField textRelleno;
-	private JTextField textRelleno1;
 	
 	private  Usuario usuario;
 
@@ -51,11 +48,12 @@ public class PanelMenuFoto extends JPanel {
 		setLayout(new BorderLayout(0, 0));
 		
 		final JPanel panelPrincipal = new JPanel();
+		panelPrincipal.setBackground(new Color(176, 196, 222));
 		add(panelPrincipal);
 		GridBagLayout gbl_panelPrincipal = new GridBagLayout();
-		gbl_panelPrincipal.columnWidths = new int[]{122, 91, 202, 85, 119, 0};
+		gbl_panelPrincipal.columnWidths = new int[]{39, 202, 0};
 		gbl_panelPrincipal.rowHeights = new int[]{282, 57, 40, 56, 0, 0};
-		gbl_panelPrincipal.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panelPrincipal.columnWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		gbl_panelPrincipal.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panelPrincipal.setLayout(gbl_panelPrincipal);
 		
@@ -65,24 +63,13 @@ public class PanelMenuFoto extends JPanel {
 		gbc_panelImagen.fill = GridBagConstraints.BOTH;
 		gbc_panelImagen.gridx = 2;
 		gbc_panelImagen.gridy = 1;
-		
-		textRelleno = new JTextField();
-		textRelleno.setBackground(SystemColor.textHighlight);
-		textRelleno.setEditable(false);
-		GridBagConstraints gbc_textRelleno = new GridBagConstraints();
-		gbc_textRelleno.insets = new Insets(0, 0, 5, 5);
-		gbc_textRelleno.fill = GridBagConstraints.BOTH;
-		gbc_textRelleno.gridx = 1;
-		gbc_textRelleno.gridy = 0;
-		panelPrincipal.add(textRelleno, gbc_textRelleno);
-		textRelleno.setColumns(10);
 	
 		
 		lblFoto = new JLabel(); 
 		GridBagConstraints gbc_lblFoto = new GridBagConstraints();
 		gbc_lblFoto.fill = GridBagConstraints.BOTH;
-		gbc_lblFoto.insets = new Insets(0, 0, 5, 5);
-		gbc_lblFoto.gridx = 2;
+		gbc_lblFoto.insets = new Insets(0, 0, 5, 0);
+		gbc_lblFoto.gridx = 1;
 		gbc_lblFoto.gridy = 0;
 		panelPrincipal.add(lblFoto, gbc_lblFoto);
 		
@@ -100,24 +87,11 @@ public class PanelMenuFoto extends JPanel {
 		Icon icono = new ImageIcon (imagen.getImage().getScaledInstance(lblFoto.getWidth(), lblFoto.getHeight(), Image.SCALE_DEFAULT));
 		lblFoto.setIcon(icono);
 		
-		//--------------------------
-		
-		textRelleno1 = new JTextField();
-		textRelleno1.setEditable(false);
-		textRelleno1.setBackground(SystemColor.textHighlight);
-		GridBagConstraints gbc_textRelleno1 = new GridBagConstraints();
-		gbc_textRelleno1.insets = new Insets(0, 0, 5, 5);
-		gbc_textRelleno1.fill = GridBagConstraints.BOTH;
-		gbc_textRelleno1.gridx = 3;
-		gbc_textRelleno1.gridy = 0;
-		panelPrincipal.add(textRelleno1, gbc_textRelleno1);
-		textRelleno1.setColumns(10);
-		
 		panelBotones = new JPanel();
 		GridBagConstraints gbc_panelBotones = new GridBagConstraints();
-		gbc_panelBotones.insets = new Insets(0, 0, 5, 5);
+		gbc_panelBotones.insets = new Insets(0, 0, 5, 0);
 		gbc_panelBotones.fill = GridBagConstraints.BOTH;
-		gbc_panelBotones.gridx = 2;
+		gbc_panelBotones.gridx = 1;
 		gbc_panelBotones.gridy = 1;
 		panelPrincipal.add(panelBotones, gbc_panelBotones);
 		
@@ -161,46 +135,24 @@ public class PanelMenuFoto extends JPanel {
 		
 		panelBotones.add(btnCambiarSaludo);
 		
-		txtNombre = new JTextField();
-		txtNombre.setEditable(false);
-		txtNombre.setText("Nombre");
-		GridBagConstraints gbc_txtNombre = new GridBagConstraints();
-		gbc_txtNombre.insets = new Insets(0, 0, 5, 5);
-		gbc_txtNombre.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtNombre.gridx = 1;
-		gbc_txtNombre.gridy = 2;
-		panelPrincipal.add(txtNombre, gbc_txtNombre);
-		txtNombre.setColumns(10);
-		
 		textNameField = new JTextField();
 		textNameField.setEditable(false);
 		textNameField.setText(usuario.getNombre());
 		GridBagConstraints gbc_textNameField = new GridBagConstraints();
-		gbc_textNameField.insets = new Insets(0, 0, 5, 5);
+		gbc_textNameField.insets = new Insets(0, 0, 5, 0);
 		gbc_textNameField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textNameField.gridx = 2;
+		gbc_textNameField.gridx = 1;
 		gbc_textNameField.gridy = 2;
 		panelPrincipal.add(textNameField, gbc_textNameField);
 		textNameField.setColumns(10);
-		
-		txtSaludo = new JTextField();
-		txtSaludo.setEditable(false);
-		txtSaludo.setText("Saludo");
-		GridBagConstraints gbc_txtSaludo = new GridBagConstraints();
-		gbc_txtSaludo.insets = new Insets(0, 0, 5, 5);
-		gbc_txtSaludo.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txtSaludo.gridx = 1;
-		gbc_txtSaludo.gridy = 3;
-		panelPrincipal.add(txtSaludo, gbc_txtSaludo);
-		txtSaludo.setColumns(10);
 		
 		textSaludoField = new JTextField();
 		System.out.println("El saludo antes de hacer nada de nada: " + usuario.getSaludo() + " y su id es " + usuario.getId()); ///////////////////
 		textSaludoField.setText(usuario.getSaludo());
 		GridBagConstraints gbc_textSaludoField = new GridBagConstraints();
-		gbc_textSaludoField.insets = new Insets(0, 0, 5, 5);
+		gbc_textSaludoField.insets = new Insets(0, 0, 5, 0);
 		gbc_textSaludoField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textSaludoField.gridx = 2;
+		gbc_textSaludoField.gridx = 1;
 		gbc_textSaludoField.gridy = 3;
 		panelPrincipal.add(textSaludoField, gbc_textSaludoField);
 		textSaludoField.setColumns(10);
--- tds.um/src/main/java/vista/VentanaPrincipal.java
+++ tds.um/src/main/java/vista/VentanaPrincipal.java
@@ -38,6 +38,7 @@ public class VentanaPrincipal extends JFrame {
 	private JFrame ventana;
 
 	private PanelChatsRecientes pChatRec;
+	private JPanel pExtra;
 	private PanelShowCont panelShowCont;
 	private PanelConversacion panelConver;
 	private VentanaPrincipal venPrinAc;
@@ -52,8 +53,10 @@ public class VentanaPrincipal extends JFrame {
 		// La vista solo debe hablar con el controlador, esto es bastante una herejía
 		// bebe
 
+		frame.setResizable(false);
+		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(300, 40, 850, 600);
+		setBounds(0, 0, 1350, 700); //850 - 600
 		panelVentanaPrincipal = new JPanel();
 		panelVentanaPrincipal.setBorder(new EmptyBorder(5, 5, 5, 5));
 		panelVentanaPrincipal.setLayout(new BorderLayout(0, 0));
@@ -69,15 +72,15 @@ public class VentanaPrincipal extends JFrame {
 			public void actionPerformed(ActionEvent arg0) {
 
 				PanelMenuFoto nuevo = new PanelMenuFoto(usuario);
-				panelDividido.remove(panellzq);
-				panellzq = nuevo;
+				panelDividido.remove(pExtra);
+				pExtra = nuevo;
 
-				GridBagConstraints gbc_panelzq = new GridBagConstraints();
-				gbc_panelzq.insets = new Insets(0, 0, 5, 0);
-				gbc_panelzq.fill = GridBagConstraints.BOTH;
-				gbc_panelzq.gridx = 1;
-				gbc_panelzq.gridy = 0;
-				panelDividido.add(nuevo, gbc_panelzq);
+				GridBagConstraints gbc_pExtra = new GridBagConstraints();
+				gbc_pExtra.insets = new Insets(0, 0, 5, 0);
+				gbc_pExtra.fill = GridBagConstraints.BOTH;
+				gbc_pExtra.gridx = 2;
+				gbc_pExtra.gridy = 0;
+				panelDividido.add(pExtra, gbc_pExtra);
 
 				panelDividido.revalidate();
 				panelDividido.repaint();
@@ -193,7 +196,7 @@ public class VentanaPrincipal extends JFrame {
 		});
 		menuBar.add(btnCuenta);
 
-		Component horizontalStrut_1 = Box.createHorizontalStrut(254);
+		Component horizontalStrut_1 = Box.createHorizontalStrut(860);
 		menuBar.add(horizontalStrut_1);
 
 		JMenu mnOpciones2 = new JMenu("Opciones2");
@@ -304,9 +307,9 @@ public class VentanaPrincipal extends JFrame {
 		// JPanel panelDividido = new JPanel();
 		panelVentanaPrincipal.add(panelDividido, BorderLayout.CENTER);
 		GridBagLayout gbl_panelDividido = new GridBagLayout();
-		gbl_panelDividido.columnWidths = new int[] { 251, 473, 0 };
+		gbl_panelDividido.columnWidths = new int[] { 251, 742, 226, 0 };
 		gbl_panelDividido.rowHeights = new int[] { 488, 0, 0 };
-		gbl_panelDividido.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
+		gbl_panelDividido.columnWeights = new double[] { 0.0, 1.0, 1.0, Double.MIN_VALUE };
 		gbl_panelDividido.rowWeights = new double[] { 1.0, 0.0, Double.MIN_VALUE };
 		panelDividido.setLayout(gbl_panelDividido);
 
@@ -329,12 +332,21 @@ public class VentanaPrincipal extends JFrame {
 
 		// JPanel panellzq = new JPanel();
 		GridBagConstraints gbc_panelzq = new GridBagConstraints();
-		gbc_panelzq.insets = new Insets(0, 0, 5, 0);
+		gbc_panelzq.insets = new Insets(0, 0, 5, 5);
 		gbc_panelzq.fill = GridBagConstraints.BOTH;
 		gbc_panelzq.gridx = 1;
 		gbc_panelzq.gridy = 0;
 		panellzq.setBackground(new Color(135, 206, 250));
 		panelDividido.add(panellzq, gbc_panelzq);
+		
+		pExtra = new JPanel();
+		pExtra.setBackground(new Color(176, 196, 222));
+		GridBagConstraints gbc_pExtra = new GridBagConstraints();
+		gbc_pExtra.insets = new Insets(0, 0, 5, 0);
+		gbc_pExtra.fill = GridBagConstraints.BOTH;
+		gbc_pExtra.gridx = 2;
+		gbc_pExtra.gridy = 0;
+		panelDividido.add(pExtra, gbc_pExtra);
 
 	}
 
